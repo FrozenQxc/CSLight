05e163e295e7a2f5a0485574626705d2010c97bd
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-03-29 11:32:08 +0300

    SystemDamage

---
+++ SystemDamage/SystemDamage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Damage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float health;
+            int armor, damage;
+            int precentConvert = 100;
+
+            Console.Write("Введите кол-во здоровья:");
+            health = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во брони:");
+            armor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во урона:");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            health -= Convert.ToSingle(damage) / precentConvert * armor;
+
+            Console.WriteLine($"Вам нанесли {damage} урона. У вас осталось {health} здоровья");
+
+            Console.ReadKey();
+        }
+    }
+}
+
