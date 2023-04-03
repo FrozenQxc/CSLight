4bcb7f79f79667d243406fce42b604d999505eec
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-03 20:43:20 +0300

    up

---
+++ App/App/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //измение окна 
+            this.Size = new System.Drawing.Size(400, 900);
+
+            this.Text = "Андрей";
+        }
+    }
+}
